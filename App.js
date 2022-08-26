03c3bcafce43f62045d6918f8a4d577e70343a8c
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-08-26 22:58:40 +0600

    FlatList

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React,{useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput,Button,ScrollView } from 'react-native';
+import { StyleSheet, Text, View,TextInput,Button,FlatList } from 'react-native';
 import ListItem from './components/ListItem/ListItem';
-import { FlatList } from 'react-native-web';
+
 
 
 export default function App() {
   const {container, colors,inputView} = styles;
   const [inputValue, setInputValue] = useState("");
   const [placeList, setPlaceList] = useState([]);
-  const list = placeList.map((item, i) => {
-    return(
-      <ListItem placeName={item} key={i} onItemPressed={() => alert(item)}></ListItem>
-    )
-  })
   return (
     <View style={container}>
       <View style={inputView}>
@@ -28,18 +23,23 @@ export default function App() {
         title= "Add"
         onPress={() =>{
           if(inputValue!==""){
-          setPlaceList([...placeList,inputValue])
+          setPlaceList([...placeList,{key:Math.random().toString(), value:inputValue}])
           }
         }} />
         
         
       </View>
         
-      <ScrollView
-      style={{width: "100%"}}
-      >
-        {list}
-      </ScrollView>
+      <FlatList style={{
+        width:"100%"
+      }}
+      data = {placeList}
+      renderItem={info =>{
+        return(
+          <ListItem placeName={info.item.value} onItemPressed={() => alert(item)}/>
+        )
+      }}
+      />
       
       </View>
     
